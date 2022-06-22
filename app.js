af658072a6ca6a219b0b744213baaecbcb27f67f
Johnson ojo <johnsonojo2@gmail.com>
2022-06-21 21:31:11 -0600

    update JS file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+"use strict";
 // const p1 = {
 //     score: 0,
 //     button: document.querySelector('#p1Button'),
@@ -51,3 +52,38 @@
 //         p.button.disabled = false;
 //     }
 // }
+
+const p1Button = document.querySelector("#p1button");
+const p2Button = document.querySelector("#p2button");
+const p1Display = document.querySelector("#p1display");
+const p2Display = document.querySelector("#p2display");
+
+let p1Score = 0;
+let p2Score = 0;
+let gameLimit = 5;
+let isGameOver = false;
+
+p1Button.addEventListener("click", () => {
+  if (isGameOver === false) {
+    p1Score += 1;
+    p1Display.innerText = p1Score;
+    if (p1Score === gameLimit) {
+      window.alert("p1 won");
+      isGameOver = true;
+    }
+  }
+});
+p2Button.addEventListener("click", () => {
+  if (!isGameOver) {
+    p2Score += 1;
+    if (p2Score === gameLimit) {
+      window.alert("p2 won");
+      isGameOver = true;
+    }
+    p2Display.innerText = p2Score;
+  }
+});
+
+if (!isGameOver) {
+  console.log(!isGameOver);
+}
